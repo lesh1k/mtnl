10a41e7c2e180e8f87c0dfbd9838842942aa16bb
Alexei Dvorac <lexhxas@gmail.com>
2016-06-04 21:52:47 +0300

    First working prototype ready.

--- mtnl.js
+++ mtnl.js
@@ -1,15 +1,91 @@
 'use strict';
 
-const CONFIG = require('./config.json');
+const LOADED_CONFIG = require('./config.json');
 
 
-function mtnl(quantity, unit_type='dry') {
+function mtnl(quantity, unit_type='dry', CONFIG=LOADED_CONFIG) {
+    const rates = CONFIG.rates;
+    const error = CONFIG.error / 100;
     const units = CONFIG.units[unit_type];
+
     if (!units){
-        throw new Error(`Unit type: ${unit_type} not present in config`);
+        throw new Error(`Unit type: ${unit_type} not present in config.units`);
+    }
+
+    if (quantity < 0) {
+        throw new Error('Quantity should be a non-negative number');
+    }
+
+    let matching_units = findMatchingWithinError(quantity, units, rates, error);
+    if (matching_units.length) {
+        let unit = findBiggestMatching(matching_units, rates);
+        let [amount, error] = getMeasure(quantity, rates[unit], units[unit]);
+        return formatResult(amount, units[unit]);
     }
 
     return `${quantity} ${CONFIG.default_unit}`;
+}
+
+function findMatchingWithinError(quantity=0, units={}, rates={}, allowed_error=0) {
+    let result = [];
+    for (let k in rates) {
+        if (!(k in units)) {
+            continue;
+        }
+
+        let [amount, actual_error] = getMeasure(quantity, rates[k], units[k]);
+        if (amount >= units[k].minimum && actual_error <= allowed_error) {
+            result.push(k);
+        }
+    }
+    return result;
+}
+
+function findBiggestMatching(matching, rates) {
+    let result = matching[0],
+        max = rates[matching[0]];
+
+    for (let k of matching) {
+        if (rates[k] > max) {
+            result = k;
+            max = rates[k];
+        }
+    }
+
+    return result;
+}
+
+function getMeasure(q, rate, unit) {
+    let amount = Math.round(q / rate / unit.divisible) * unit.divisible,
+        error = Math.abs(q - amount * rate) / q;
+
+    return [amount, error];
+}
+
+function formatResult(amount, unit) {
+    let full = parseInt(amount),
+        partial = parseInt((amount - full) / unit.divisible),
+        res = '';
+
+    if (full) {
+        res += `${full} `;
+    }
+
+    if (partial) {
+        let numerator = partial,
+            denominator = parseInt(1/unit.divisible),
+            common_factor = denominator / numerator;
+
+        if (common_factor % 1 === 0 && numerator / common_factor % 1 === 0) {
+            numerator = numerator / common_factor;
+            denominator = denominator / common_factor;
+        }
+
+        res += `${numerator}/${denominator} `;
+    }
+
+    res += `${unit.name}`;
+    return res;
 }
 
 module.exports = mtnl;
--- test/mtnl.tests.js
+++ test/mtnl.tests.js
@@ -1,22 +1,55 @@
 /* eslint-env mocha */
-let chai = require('chai');
-let assert = chai.assert;
+let assert = require('chai').assert;
 
-const CONFIG = require('../config.json');
-let mtnl = require('../mtnl');
+const TEST_CONFIG = require('./tests.config.json');
+const mtnl = function(quantity, unit_type='dry', config=TEST_CONFIG) {
+    return require('../mtnl').call(null, quantity, unit_type, config);
+};
+
 
 describe('MTNL', () => {
     it('Is a function', () => {
         assert.isFunction(mtnl);
     });
 
-    it('Returns input quantity with default_unit if no result available', () => {
-        let expected = `-1 ${CONFIG.default_unit}`;
-        assert.strictEqual(mtnl(-1), expected);
+    it('Throws an err if quantity is negative', () => {
+        let fn = mtnl.bind(null, -1);
+        assert.throws(fn, Error);
     });
 
     it('Throws an err if unit not in config', () => {
         let fn = mtnl.bind(null, 100, 'THE IMPROBABLE UNIT');
         assert.throws(fn, Error);
+    });
+
+    describe('Calculations', () => {
+        it('Returns 1 gram(s) for input 1', () => {
+            assert.strictEqual(mtnl(1), '1 gram(s)');
+        });
+
+        it('Takes into consideration minimum amount of a unit', () => {
+            // Should choose "2 cup(s)" over "1/2 liter(s)" because liter has a minimum of 1
+            assert.strictEqual(mtnl(500, 'volume'), '2 cup(s)');
+        });
+
+        it('Returns the "Rate-unit" if quantity is strictEqual to unit\'s capacity', () => {
+            let CONFIG = require('./tests.config.json');
+            // Set 0 error tolerance
+            CONFIG.error = 0;
+
+            for (let unit_type in CONFIG.units) {
+                for (let unit in CONFIG.units[unit_type]) {
+                    assert.strictEqual(
+                        mtnl(CONFIG.rates[unit], unit_type, CONFIG),
+                        `1 ${CONFIG.units[unit_type][unit].name}`,
+                        `mtnl(${CONFIG.rates[unit]}, ${unit_type})`
+                    );
+                }
+            }
+        });
+
+        it('Simplifies fractions if common factor available (e.g. 2/4 -> 1/2)', () => {
+            assert.strictEqual(mtnl(2.5, 'volume'), '1/2 teaspoon(s)');
+        });
     });
 });
